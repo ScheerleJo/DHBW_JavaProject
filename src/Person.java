f8e9e023a59e3d42452fa0e3807def1972512310
Josia <scheerle.josia@gmail.com>
2024-06-12 23:24:01 +0200

    Start implementing Data Modell

---
+++ src/Person.java
@@ -0,0 +1,26 @@
+public class Person {
+    private int id;
+    private String name;
+    private enum role;
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }/*
+    public void setRole(enum role) {
+        this.role = role;
+    }
+
+    public enum getRole() {
+        return role;
+    }*/
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+}
