ae6379248d9794c9ec7e52c26f426c64047ff9a0
Josia <scheerle.josia@gmail.com>
2024-06-18 22:24:58 +0200

    Start Importing and Action handling

--- src/LoadDB.java
+++ src/LoadDB.java
@@ -1,4 +1,36 @@
 public class LoadDB {
+    private static final String dbPath = System.getProperty("user.dir") + "\\movieproject2024.db";
 
-    
+    public String getDbPath() {
+        return dbPath;
+    }
+
+
+    public void importDB(){
+
+    }
+
+    public void handleAction(String[] args) {
+        switch (args[0]){
+            case "--filmsuche":
+                System.out.println("Film Suche");
+                break;
+            case "--schauspielersuche":
+                System.out.println("Schauspieler Suche");
+                break;
+            case "--filmnetzwerk":
+                System.out.println("Filmnetzwerk");
+                break;
+            case "--schauspielernetzwerk":
+                System.out.println("Schauspielernetzwerk");
+                break;
+        }
+    }
+
+
+
+
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if(args.length == 0) {
+            System.out.println("No Parameter Specified");
+        } else {
+            LoadDB loadDB = new LoadDB();
+
+            loadDB.importDB();
+            loadDB.handleAction(args);
+        }
     }
 }
